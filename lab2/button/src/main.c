555153e6eee5a1eb607607369548d841489a47fe
Brad Campbell <bradjc5@gmail.com>
2023-02-13 18:42:27 -0500

    add button example

---
+++ lab2/button/src/main.c
@@ -0,0 +1,88 @@
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/gpio.h>
+#include <sys/util.h>
+#include <sys/printk.h>
+#include <inttypes.h>
+
+#define SW0_NODE	DT_ALIAS(sw0)
+#define SW1_NODE	DT_ALIAS(sw1)
+#define SW2_NODE	DT_ALIAS(sw2)
+#define SW3_NODE	DT_ALIAS(sw3)
+
+static const struct gpio_dt_spec button1 = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+static const struct gpio_dt_spec button2 = GPIO_DT_SPEC_GET_OR(SW1_NODE, gpios, {0});
+static const struct gpio_dt_spec button3 = GPIO_DT_SPEC_GET_OR(SW2_NODE, gpios, {0});
+static const struct gpio_dt_spec button4 = GPIO_DT_SPEC_GET_OR(SW3_NODE, gpios, {0});
+
+static struct gpio_callback button1_cb_data;
+static struct gpio_callback button2_cb_data;
+static struct gpio_callback button3_cb_data;
+static struct gpio_callback button4_cb_data;
+
+
+
+void button1_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	int val = gpio_pin_get_dt(&button1);
+	printk("Button1 %i\n", val);
+}
+
+void button2_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	int val = gpio_pin_get_dt(&button3);
+	printk("Button2 %i\n", val);
+}
+
+void button3_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	int val = gpio_pin_get_dt(&button3);
+	printk("Button3 %i\n", val);
+}
+
+void button4_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	int val = gpio_pin_get_dt(&button4);
+	printk("Button4 %i\n", val);
+}
+
+
+void init_button(const struct gpio_dt_spec* button,
+                 struct gpio_callback* button_cb_data,
+				 gpio_callback_handler_t cb) {
+	int ret;
+
+	if (!device_is_ready(button->port)) {
+		printk("Error: button device %s is not ready\n",
+		       button->port->name);
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button->port->name, button->pin);
+		return;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(button, GPIO_INT_EDGE_BOTH);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button->port->name, button->pin);
+		return;
+	}
+
+	gpio_init_callback(button_cb_data, cb, BIT(button->pin));
+	gpio_add_callback(button->port, button_cb_data);
+	printk("Set up button at %s pin %d\n", button->port->name, button->pin);
+}
+
+void main(void)
+{
+	init_button(&button1, &button1_cb_data, button1_pressed);
+	init_button(&button2, &button2_cb_data, button2_pressed);
+	init_button(&button3, &button3_cb_data, button3_pressed);
+	init_button(&button4, &button4_cb_data, button4_pressed);
+
+	printk("Press any button\n");
+}
