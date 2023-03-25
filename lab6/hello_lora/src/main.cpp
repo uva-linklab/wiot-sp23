0200441d3618a0ada283ee9fc7c9d6fa2ff9a2b6
Viswajith <89476243+viswajith-g@users.noreply.github.com>
2023-03-25 11:51:59 -0400

    update for prelab

--- lab6/hello_lora/src/main.cpp
+++ lab6/hello_lora/src/main.cpp
@@ -4,43 +4,30 @@
 
 #define LED 35
 
+int counter = 0;
 const uint64_t TIMER_PERIOD_US = 1000000;
 
-void toggle_led(int count){
-  for (int i = 0; i < count; i++){
-    digitalWrite(LED, HIGH);
-    delay(1000);
-    digitalWrite(LED, LOW);
-  }
-}
-
 void IRAM_ATTR timerISR(void* arg) {
-    // do something here
+  counter++;
+  Serial.println("Hello LoRa!: " + String(counter));
 }
-
-
-/*************************
- * 
- * The second argument for the timerBegin function is the prescaler value. In this case, we are using a prescaler of 1024, which lets us 
- * run the device at 16MHz. The base clock frequency is 15.625KHz. 
- * 
-**************************/
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello LoRa!");
+  pinMode(LED, OUTPUT);
 
   esp_timer_handle_t timer;
   esp_timer_create_args_t timer_args = {
       .callback = &timerISR,
       .arg = NULL,
       .dispatch_method = ESP_TIMER_TASK,
-      .name = "<whatever you want to name your timer>"
+      .name = "Hello LoRa Timer Printer"  //<whatever you want to name your timer>
   };
+
   esp_timer_create(&timer_args, &timer); 
-   //esp_timer_start_periodic(<timer handle>, <period>);
-  
-  
+
+  esp_timer_start_periodic(timer, TIMER_PERIOD_US);  
 }
 
 void loop() {
